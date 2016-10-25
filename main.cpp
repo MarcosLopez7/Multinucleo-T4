c65cdad75de659d43ccf3bf09b4781ab0070deff
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-10-25 18:34:16 -0500

    Antes que se me vaya el internet

--- main.cpp
+++ main.cpp
@@ -7,19 +7,21 @@
 using namespace cv;
 using namespace std;
 
-int suma(int *);
+int suma(int *); //Función que va ejecutar la suma de los puntos con el filtro, regresando el resultado de la suma, y recibiendo 
+//un arreglo con los puntos que van hacer la suma
 
-int filtro[9] = {0 , 1, 0, 1, -4, 1, 0, 1, 0};
+int filtro[9] = {1 , 1, 1, 1, -8, 1, 1, 1, 1}; //Filtro o máscara para la suma de puntos laplaceana
 
 int main( int argc, char** argv )  {
 
-    Mat src, gray, dst;
-    src = imread( "salon.jpg" );
-    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT ); /// Remove noise by blurring with a Gaussian filter
-    cvtColor( src, gray, CV_RGB2GRAY );
-    dst = gray.clone();
-    int *pedazoDeMatriz = (int *) malloc(3 * 3 * sizeof(int));
+    Mat src, gray, dst; //Variable Mat que guarda el src, los puntos de la imagen original, gray para transformar a grises la imagen y dst como la imagen resultado de la suma laplaceana
+    src = imread( "salon.jpg" ); //Carga de la imagen
+    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT ); /// Quitando el 'ruido' haciendo un filtro gausseano de OpenCV, esto para facilitar la detección de puntos
+    cvtColor( src, gray, CV_RGB2GRAY ); //conversión de la imagen a grises
+    dst = gray.clone(); //clonar puntos para dst para que tenga el tamañao de la imagen original
+    int *pedazoDeMatriz = (int *) malloc(3 * 3 * sizeof(int)); //Un arreglo que va guardar los puntos de alrededor del punto para la suma
 
+    //For para hacer la suma en todos los puntos
     for (int i = 1; i < gray.rows - 1; ++i)
         for (int j = 1; j < gray.cols - 1; ++j) {
             pedazoDeMatriz[0] = gray.at<uchar>(i-1, j-1);
@@ -31,27 +33,29 @@ int main( int argc, char** argv )  {
             pedazoDeMatriz[6] = gray.at<uchar>(i-1, j+1);
             pedazoDeMatriz[7] = gray.at<uchar>(i, j+1);
             pedazoDeMatriz[8] = gray.at<uchar>(i+1, j+1);
-            dst.at<uchar>(i, j) = suma(pedazoDeMatriz);
+            dst.at<uchar>(i, j) = suma(pedazoDeMatriz); //Llamar función suma con la matriz, regresar y guardarla en los puntos de la imagen resultado
         }
 
-    imshow("Original", src);
-    imshow( "Resultado", dst );
-    free(pedazoDeMatriz);
+    imshow("Original", src); //carga de la imagen original
+    imshow( "Resultado", dst ); //carga de la imagen resultado
+    free(pedazoDeMatriz); //liberar el arreglo temporal
     waitKey(0);
     return 0;
 }
 
+//Función de la suma laplaceana
 int suma(int *m) {
 
-    int res = 0;
+    int res = 0; //inicialización del resultado
 
+    //suma de los puntos aplicando el filtro laplaceano
     for (int i = 0; i < 9; ++i)
         res += m[i] * filtro[i];
 
     if (res > 255)
-        return 255;
+        return 255; //Unidad máxima para rgb 255, si es más devolver el valor máximo
     else if (res < 0)
-        return 0;
+        return 0; //UNidad mínima para rgb 0, si es menor devolver 0
     else
-        return res;
+        return res; //retorno del valor de la suma si no fue mayor a 255 y no fue menor de 0
 }
--- main.cu
+++ main.cu
@@ -9,35 +9,41 @@
 using namespace cv;
 using namespace std;
 
-int suma(int *);
-
+//Kernel de CUDA para la función de la suma laplaceana em 1 dimensión, recibe una matriz con los puntos de la imagen general, la matriz de resultado, w como el ancho de la imagen, h como el largo de la imagen
 __global__ void laplaceadno1D(int *src, int *dst, int w, int h) {
 	
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    int filtro[9] = {1 , 1, 1, 1, -8, 1, 1, 1, 1};
+    int i = blockIdx.x*blockDim.x + threadIdx.x; //el id del thread en el que estamos trabajando en el kernel
+    int filtro[9] = {1 , 1, 1, 1, -8, 1, 1, 1, 1}; //filtro laplaceano para la suma de puntos
 
+    //Checar que el thread este dentro de los puntos permitidos en lo alto para que no de segmentation fault
     if(i > 0 && h - 1> i)
-    	for(int j = 1; j < w - 1; j++) {
+    	for(int j = 1; j < w - 1; j++) { //for para iterar sobre lo ancho de la imagen y obtener los puntos conforme pase el ciclo
+		//variable de suma que va guardar el resultado de la suma con el filtro laplaceano
 		int suma = src[(i - 1) * w + j - 1] * filtro[0] + src[i * w + j - 1] * filtro[1] + src[(i + 1) * w + j - 1] * filtro[2];
 		suma += src[(i - 1) * w + j] * filtro[3] + src[i * w + j] * filtro[4] + src[(i + 1) * w + j] * filtro[5]; 
 		suma += src[(i - 1) * w + j + 1] * filtro[6] + src[i * w + j + 1] * filtro[7] + src[(i + 1) * w + j + 1] * filtro[8];	    
-		dst[i * w + j] = suma;
+		dst[i * w + j] = suma; //guardar resultado en la matriz de resultados
     	}
 
 }
 
+
+//Kernel de CUDA para la función de la suma laplaceana em 2 dimensión, recibe una matriz con los puntos de la imagen general, la matriz de resultado, w como el ancho de la imagen, h como el largo de la imagen
 __global__ void laplaceadno2D(int *src, int *dst, int w, int h) {
 
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    int j = blockIdx.y*blockDim.y + threadIdx.y;
+    int i = blockIdx.x*blockDim.x + threadIdx.x; //el id del thread en el que estamos trabajando en el kernel en la dimensión X
+    int j = blockIdx.y*blockDim.y + threadIdx.y; //el id del thread en el que estamos trabajando en el kernel en la dimensión Y
     int filtro[9] = {1 , 1, 1, 1, -8, 1, 1, 1, 1};
 
+    //Checar que el thread este dentro de los puntos permitidos en lo alto para que no de segmentation fault
     if(i > 0 && h - 1 > i)
+	//Checar que el thread este dentro de los puntos permitidos en lo alto para que no de segmentation fault
         if(j > 0 && j < w - 1) {
-                int suma = src[(i - 1) * h + j - 1] * filtro[0] + src[i * h + j - 1] * filtro[1] + src[(i + 1) * h + j - 1] * filtro[2];
-                suma += src[(i - 1) * h + j] * filtro[3] + src[i * h + j] * filtro[4] + src[(i + 1) * h + j] * filtro[5];
-                suma += src[(i - 1) * h + j + 1] * filtro[6] + src[i * h + j + 1] * filtro[7] + src[(i + 1) * h + j + 1] * filtro[8];
-                dst[i * w + j] = suma;
+                //variable de suma que va guardar el resultado de la suma con el filtro laplaceano
+		int suma = src[(i - 1) * w + j - 1] * filtro[0] + src[i * w + j - 1] * filtro[1] + src[(i + 1) * w + j - 1] * filtro[2];
+                suma += src[(i - 1) * w + j] * filtro[3] + src[i * w + j] * filtro[4] + src[(i + 1) * w + j] * filtro[5];
+                suma += src[(i - 1) * w + j + 1] * filtro[6] + src[i * w + j + 1] * filtro[7] + src[(i + 1) * w + j + 1] * filtro[8];
+                dst[i * w + j] = suma; //guardar resultado en la matriz de resultados
 
         }
 
@@ -45,39 +51,46 @@ __global__ void laplaceadno2D(int *src, int *dst, int w, int h) {
 
 int main( int argc, char** argv )  {
 
-    Mat src, gray, dst;
-    src = imread( "salon.jpg" );
-    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT ); /// Remove noise by blurring with a Gaussian filter
-    cvtColor( src, gray, CV_RGB2GRAY );
-    dst = gray.clone();
-    int *srcA = (int *) malloc(src.rows * src.cols * sizeof(int));
-    int *dstA = (int *) malloc(src.rows * src.cols * sizeof(int));
-    int *dev_src, *dev_dst;
+    Mat src, gray, dst;//Variable Mat que guarda el src, los puntos de la imagen original, gray para transformar a grises la imagen y dst como la imagen resultado de la suma laplaceana
+    src = imread( "salon.jpg" );//Carga de la imagen
+    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT ); /// Quitando el 'ruido' haciendo un filtro gausseano de OpenCV, esto para facilitar la detección de puntos
+    cvtColor( src, gray, CV_RGB2GRAY );//conversión de la imagen a grises
+    dst = gray.clone();//clonar puntos para dst para que tenga el tamañao de la imagen original
+    int *srcA = (int *) malloc(src.rows * src.cols * sizeof(int)); //inicialización de matriz que va copiar los puntos de la imagen original para luego pasarlo a un arreglo de CUDA
+    int *dstA = (int *) malloc(src.rows * src.cols * sizeof(int)); //Inicialización de la matriz que va contener los resultados de la suma de la imagen y va recibir la copia del arreglo de CUDA
+    int *dev_src, *dev_dst; //inicialización de los arreglos que va cargar los puntos pero en los kernel de CUDA
+    //inicialización de la memoria
     cudaMalloc( (void**)&dev_src, src.rows * src.cols * sizeof(int) );
     cudaMalloc( (void**)&dev_dst, src.rows * src.cols * sizeof(int) );
 
+    //verificar que haya 3 argumentos a la hora de ejcutar el programa
     if (argc != 4){
 	cout << "Se esperaba la opcion, el numero de bloques y threads, en ese orden\n";	
 	exit(-1);
     }
  
+    //Variables que toman los parametros de ejecución del programa
     int opcion = atoi(argv[1]);
     int blocks = atoi(argv[2]);    
     int threads = atoi(argv[3]);
 
+    //Copia de los puntos de la imagen original al arreglo
     for(int i = 0; i < src.rows; i++) 
 	for (int j = 0; j < src.cols; j++)
 	    srcA[i * src.cols + j] = src.at<uchar>(i, j);
 
+    //Copia de los puntos de la imagen que están en el arreglo al arreglo de CUDA
     cudaMemcpy( dev_src, srcA, src.rows * src.cols * sizeof(int), cudaMemcpyHostToDevice );
 
-    if (opcion == 1) 
+    if (opcion == 1) //Ejecución de la función de la suma en 1 dimensión
 	laplaceadno1D<<<blocks, threads>>>(dev_src, dev_dst, src.cols, src.rows);
-    else if(opcion == 2)
+    else if(opcion == 2) //Ejecución de la función de la suma en 2 dimensiones
 	laplaceadno2D<<<blocks, threads>>>(dev_src, dev_dst, src.cols, src.rows);    	
 
+    //Copia del arreglo de resultado de la suma de CUDA al arreglo 
     cudaMemcpy( dstA, dev_dst, src.rows * src.cols * sizeof(int), cudaMemcpyDeviceToHost );
 
+    //For que va copiar los resultados de los puntos del arreglo a una variable Mat para que sea
     for(int i = 0; i < src.rows; i++) {
         for (int j = 0; j < src.cols; j++){
 	    if (dstA[i * src.cols + j] > 255)
